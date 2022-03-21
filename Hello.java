7c7c6405d40ad86d255201e369fc3c7c705f3586
Mars <57082328+JuneRainBlog@users.noreply.github.com>
2022-03-21 15:17:08 +0800

    Upload by PicGo

---
+++ Hello.java
@@ -0,0 +1,21 @@
+public class Hello{
+    public static int i =12;
+    int z = 12;
+    public static void main(String[] args){
+        System.out.println("HELLO");
+    }
+    public void test1(int b){
+    
+        int a = b;
+    }
+    public static void test2(int b){
+        int a = 12;
+    }
+    public  void test3(Object o){
+        int a = 12;
+    }
+    public  void test4(int[] b){
+        Long a = 1000L;
+        int c = z;
+    }
+}
